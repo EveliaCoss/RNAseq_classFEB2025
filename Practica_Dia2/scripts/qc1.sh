99baa48be3260a84d65642151bbd5bdd7e2409da
EveliaCoss <y0kocrozz@gmail.com>
2025-02-26 00:42:08 -0600

    agregar datos

--- Practica_Dia2/scripts/qc1.sh
+++
@@ -1,4 +0,0 @@
-# sustituye los paths con los tuyos
-for file in /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/data/*.fastq.gz; do fastqc $file -o /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/quality1; done
-cd /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/quality1
-multiqc .
