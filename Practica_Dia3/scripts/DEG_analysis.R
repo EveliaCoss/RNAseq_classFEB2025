3c2d120fc21fdacab7e0e8fa328a83439082c04f
EveliaCoss <y0kocrozz@gmail.com>
2025-03-03 20:54:24 -0600

    clase 3 actualizacion

--- Practica_Dia3/scripts/DEG_analysis.R
+++ Practica_Dia3/scripts/DEG_analysis.R
@@ -1,10 +1,10 @@
 ######
 # Script : Analisis de expresion diferencial
-# Author: Sofia Salazar, Diego Ramirez y Evelia Coss
-# Date: 27/02/2024
+# Author: Sofia Salazar y Evelia Coss
+# Date: 03/03/2025
 # Description: El siguiente script nos permite realiza el Analisis de expresion Diferencial
 # a partir de los datos provenientes del alineamiento de STAR a R,
-# Primero correr el script "load_data_inR.R"
+# Primero correr el script "import_txi_inR.R"
 # Usage: Correr las lineas en un nodo de prueba en el cluster.
 # Arguments:
 #   - Input: Cargar la variable raw_counts.RData que contiene la matriz de cuentas y la metadata
@@ -17,143 +17,163 @@
 
 # --- Load packages ----------
 library(DESeq2)
+library(ggplot2)
+library(tidyverse)
+library(cowplot)
+library(reshape2)
 
 # --- Load data -----
 # Cargar archivos
-outdir <- "/mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/results/"
-figdir <- '/mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/results/figures/'
+workdir <- 'RNA_lupus/'
+load(file = paste0(workdir, 'counts/txi_filtered.RData'))
+outdir = 'RNA_lupus/'
 
-#Cargar variable "counts", proveniente del script "load_data_inR.R"
-load("/mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/results/counts/raw_counts.RData") 
-samples <- metadata$sample_id # Extraer los nombres de los Transcriptomas
-metadata$type <- as.factor(metadata$type) # convertir a factor
+metadata <- read.csv(file = paste0(workdir, 'metadata/metadata.csv'), header = T)
 
-# --- DEG ----
-counts <- counts[which(rowSums(counts) > 10),] #Seleccionamos genes con mas de 10 cuentas
+# Solo control
+control_df <- metadata[metadata$Group == "Ctrl",]
+dim(control_df) # [1] 50 14
 
-# Convertir al formato dds
-dds <- DESeqDataSetFromMatrix(countData =  counts, 
-            colData = metadata, design = ~type) #Se hace un DESeqDataSet para realizar un analisis
 
-dim(dds) # checar las dimensiones
-#[1] 33470     8
+## ---- Construct DESEQDataSet Object (dds object) -----
+# If you have performed transcript quantification (with Salmon, kallisto, RSEM, etc.) 
+# you could import the data with tximport, which produces a list, and then you can 
+# use DESeqDataSetFromTximport().
+
+names(filtered_txi) # columnas en Txi
+## [1] "abundance"           "counts"              "length"             
+## [4] "countsFromAbundance"
+
+dds <- DESeqDataSetFromTximport(txi = filtered_txi,
+                                colData = metadata,
+                                design = ~ Dose_category + Cell_type + SLEDAI_category)
+dds
+
+# using counts and average transcript lengths from tximport
+
+#class: DESeqDataSet
+#dim: 29744 165
+#metadata(1): version
+#assays(2): counts avgTxLength
+#rownames(29744): A1BG A1BG-AS1 ... ZZEF1 ZZZ3
+#rowData names(0):
+#  colnames(165): QR011_0 QR011_1 ... QR108_3 QR108_4
+#colData names(7): sample_ID Group ... Cell_type Internal
+
+dim(dds)
+# [1] 21816   165
+
+## ---- Normalizacion de los datos -----
+counts <- assays(dds)$counts
+
+# > vsd
+vsd <- varianceStabilizingTransformation(counts)
+head(vsd)
+
+# Save vsd
+save(vsd, file = paste0(outdir, "vsd.RData"))
+
+# > vsd all info
+vsd_all <- vst(dds, blind=FALSE)
+
+# Save vsd
+save(vsd_all, file = paste0(outdir, "vsd_all.RData"))
+
+# > Melting dataset for visualization
+melted_norm_counts <-data.frame(melt(assay(vsd_all)))
+# rename columns
+colnames(melted_norm_counts) <- c("gene", "sample_ID", "normalized_counts")
+# join metadata
+melted_norm_counts <- left_join(melted_norm_counts, metadata, by = "sample_ID")
+
+# > z-score
+var_non_zero <- apply(counts, 1, var) !=0 #  filter out genes that have low variance across samples
+filtered_counts <- counts[var_non_zero, ]
+
+zscores <- t(scale(t(filtered_counts)))
+dim(zscores) #[1] 18782   165
+
+zscore_mat <- as.matrix(zscores)
+
+# Save melted_norm_counts with z-scores
+save(zscore_mat, file = paste0(outdir, "zscore_mat.RData"))  
+
+
+# ------ Plot PCA -------
+# Obtain matrix
+mat <- as.matrix(vsd) # col = samples, rows  = genes
+
+# PCA (principal components) analysis
+pc <- prcomp(t(mat)) # col = genes , rows = samples
+
+pca_df <- cbind(pc$x[,1:2], metadata[,c(2,7)]) %>% as.data.frame()
+pca_df$sample <- rownames(pca_df)
+
+# Acomodar levels
+pca_df$Group <- as.factor(pca_df$Group)
+levels(pca_df$Group)
+# [1] "Ctrl" "SLE" 
+
+levels(pca_df$Cell_type)
+# [1] "moDC"     "moDCIMQ"  "monocyte" "tolDC"    "tolDCIMQ"
+
+# Agregar varianza
+variance_explained <- pc$sdev^2 / sum(pc$sdev^2) * 100
+
+p <- ggplot(pca_df, aes(x = PC1, y = PC2, color = Cell_type)) +
+  stat_ellipse() +  # Elipse por 'Cell_type'
+  geom_point(aes(shape = Group), size = 2.5) +  # Mantener color por 'Group', pero no en la elipse
+  scale_shape_manual(values=c(0, 16)) + 
+  scale_color_manual(values = c('palevioletred','#256139','black','gold1','dodgerblue3')) +
+  labs(x = sprintf('Principal Component 1 (%.2f%%) of variance', variance_explained[1]),
+       y = sprintf('Principal Component 2 (%.2f%%) of variance',variance_explained[2]),
+       color = "Cell type",
+       shape = "Group") +
+  theme_minimal() +
+  theme(plot.background = element_rect(fill = "white"),
+        text = element_text(size = 12),
+        axis.title = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.key.size = unit(1.5, "lines"))
+
+p
+
 
 ##  -- Asignar la referencia y generar contrastes -----
-# Las comparaciones se realizan por pares
-#Si no se indica de manera explicita que se va a comparara, lo va a tomar de manera alfabetica, 
-# en este caso se indica que control es la referencia, 
-dds$type <- relevel(dds$type, ref = "CONTROL") 
+# Select only control
+dds_control <- dds[, control_df$sample_ID] # select columns
+dim(dds_control)
+# [1] 21816    50
 
-## --- Obtener archivo dds ----
+copy_dds <- dds_control
+## --- Differential expression analysis (Design 1) ----
+# Design 1
+# Add comparison
+dds_control$Cell_type <-  relevel(dds_control$Cell_type, ref = 'monocyte')
+# add design
+design(dds_control) <- ~ Cell_type # only cell type because dose is the same
+design(dds_control)
 
-dds <- DESeq(dds)
-
+# Correr analisis
+dds_control <- DESeq(dds_control)
 # estimating size factors
+# using 'avgTxLength' from assays(dds), correcting for library size
 # estimating dispersions
 # gene-wise dispersion estimates
 # mean-dispersion relationship
 # final dispersion estimates
 # fitting model and testing
+# -- replacing outliers and refitting for 64 genes
+# -- DESeq argument 'minReplicatesForReplace' = 7 
+# -- original counts are preserved in counts(dds)
+# estimating dispersions
+# fitting model and testing
 
-# Obtener la lista de coeficientes o contrastes
-resultsNames(dds)
+resultsNames(dds_control) # lists the coefficients
+# [1] "Intercept"                      "Cell_type_moDC_vs_monocyte"    
+# [3] "Cell_type_moDCIMQ_vs_monocyte"  "Cell_type_tolDC_vs_monocyte"   
+# [5] "Cell_type_tolDCIMQ_vs_monocyte"
 
-# [1] "Intercept"                 "type_PLS_15min_vs_CONTROL"
-# [3] "type_PLS_30min_vs_CONTROL" "type_PLS_4h_vs_CONTROL"
-
-# Guardar la salida del diseno
-save(metadata, dds, file = paste0(outdir, 'dds_Times_vs_control.RData'))
-
-## --- Normalizacion de los datos ---------
-# Opcion 1. log2(n + 1)
-ntd <- normTransform(dds)
-
-# Opcion 2. regularized logarithm or rlog
-# Normalizacion de las cuentas por logaritmo y podrias hacer el analisis usando este objeto en lugar del dds
-ddslog <- rlog(dds, blind = F) 
-
-# Opcion 3. vsd
-# Estima la tendencia de dispersion de los datos y calcula la varianza, hace una normalizacion de las 
-# cuentas con respecto al tamaño de la libreria
-vsdata <- vst(dds, blind = F) 
-
-## --- Deteccion de batch effect ----
-
-# Almacenar la grafica
-png(file = paste0(figdir, "PCA_rlog.png"))
-plt <- plotPCA(ddslog, intgroup = "type")
-print(plt)
-dev.off()
-
-# Almacenar la grafica
-png(file = paste0(figdir, "PCA_vsd.png"))
-plt <- plotPCA(vsdata, intgroup = "type")
-print(plt)
-dev.off()
-
-# Guardar la salida del diseno (vsdata)
-save(metadata, vsdata, file = paste0(outdir, 'vst_Times_vs_control.RData'))
-
-# En la grafica de las primeras dos componentes principales son notorias las diferencias 
-# entre tipos de muestras con respecto a las componente principales que capturan su varianza, 
-# cada componente principal representa una combinacion lineal de las variables (en este caso genes) 
-# que explican la mayor cantidad de varianza en nuestros datos (las cuentas).
-
-
-## ---- Obtener informacion del contraste 1 ----
-# results(dds, contrast=c("condition","treated","untreated"))
-res_15t <- results(dds, name = "type_PLS_15min_vs_CONTROL")
-res_15t
-
-summary(res_15t)
-
-# out of 33470 with nonzero total read count
-# adjusted p-value < 0.1
-# LFC > 0 (up)       : 1657, 5%
-# LFC < 0 (down)     : 811, 2.4%
-# outliers [1]       : 0, 0%
-# low counts [2]     : 18169, 54%
-# (mean count < 12)
-# [1] see 'cooksCutoff' argument of ?results
-# [2] see 'independentFiltering' argument of ?results
-
-# Guardar los resultados
-write.csv(res_15t, file=paste0(outdir, 'DE_15min_vs_control.csv'))
-
-## ---- Obtener informacion del contraste 2 ----
-res_30t <- results(dds, name = "type_PLS_30min_vs_CONTROL")
-res_30t
-
-summary(res_30t)
-
-# out of 33470 with nonzero total read count
-# adjusted p-value < 0.1
-# LFC > 0 (up)       : 1309, 3.9%
-# LFC < 0 (down)     : 1300, 3.9%
-# outliers [1]       : 0, 0%
-# low counts [2]     : 19467, 58%
-# (mean count < 15)
-# [1] see 'cooksCutoff' argument of ?results
-# [2] see 'independentFiltering' argument of ?results
-
-# Guardar los resultados
-write.csv(res_30t, file=paste0(outdir, 'DE_30min_vs_control.csv'))
-
-## ---- Obtener informacion del contraste 3 ----
-res_4t <- results(dds, name = "type_PLS_4h_vs_CONTROL")
-res_4t
-
-summary(res_4t)
-
-# out of 33470 with nonzero total read count
-# adjusted p-value < 0.1
-# LFC > 0 (up)       : 4289, 13%
-# LFC < 0 (down)     : 3409, 10%
-# outliers [1]       : 0, 0%
-# low counts [2]     : 6489, 19%
-# (mean count < 3)
-# [1] see 'cooksCutoff' argument of ?results
-# [2] see 'independentFiltering' argument of ?results
-
-# Guardar los resultados
-write.csv(res_4t, file=paste0(outdir, 'DE_4h_vs_control.csv'))
+# save
+save(metadata, dds_control, file = paste0(outdir, 'dds_objects/dds_control_design1.RData'))
